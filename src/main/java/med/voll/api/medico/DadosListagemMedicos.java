da3ced9c5138042cbf8e722f94e3ba23a6e24c47
sunHelen12 <sunrise.developer12@gmail.com>
2024-12-31 23:20:08 -0300

    Criação de atualização de dados dos médicos registrados - criacao de construtores e record dto com somente os dados que podem ser alterados

--- src/main/java/med/voll/api/medico/DadosListagemMedicos.java
+++ src/main/java/med/voll/api/medico/DadosListagemMedicos.java
@@ -1,12 +1,13 @@
 package med.voll.api.medico;
 
-public record DadosListagemMedicos(String  nome,
+public record DadosListagemMedicos( Long id,
+                                    String  nome,
                                     String email,
                                     String crm,
                                     Especialidade especialidade) {
     
     public DadosListagemMedicos(Medico medico){
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(),medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
 
 }
