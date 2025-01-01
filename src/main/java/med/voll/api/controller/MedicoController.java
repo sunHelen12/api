ed5903b61cc91bc0ba0b63b471365afcee78cc7a
sunHelen12 <sunrise.developer12@gmail.com>
2024-12-31 21:48:53 -0300

    Adicionando a função de paginação do método de listagem de médicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,8 +1,10 @@
 package med.voll.api.controller;
 
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,9 +32,9 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedicos> listarMedicos(){
-        return repository.findAll().stream().map(DadosListagemMedicos::new).toList();
-                                    //converter dados da C. Medico para DadosListagemMedicos DTO - e converter para uma lista
+    public Page<DadosListagemMedicos> listarMedicos(Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedicos::new);
+                                    //converter dados da C. Medico para DadosListagemMedicos DTO 
     }
 
 
