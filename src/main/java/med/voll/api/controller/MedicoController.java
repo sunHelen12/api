5cd84cf570e9952f6946b9bf2bbbd061fdccfddc
sunHelen12 <sunrise.developer12@gmail.com>
2024-12-31 22:01:48 -0300

    Adição de listagem por nome em ordem crescente e atualização da paginação dos dados

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,6 +5,7 @@ package med.voll.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,7 +33,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedicos> listarMedicos(Pageable paginacao){
+    public Page<DadosListagemMedicos> listarMedicos(@PageableDefault(size = 10, sort = {"nome"})Pageable paginacao){
         return repository.findAll(paginacao).map(DadosListagemMedicos::new);
                                     //converter dados da C. Medico para DadosListagemMedicos DTO 
     }
