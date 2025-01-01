1ea7df7ba4e2ac85b2466572a2f2b1fb8424316d
sunHelen12 <sunrise.developer12@gmail.com>
2025-01-01 15:29:23 -0300

    Criação de Tabela para pacientes - Criação de records, repository e controller para adicionar, listar, paginar e inativar dados de pacientes

---
+++ src/main/java/med/voll/api/paciente/record/DadosListagemPaciente.java
@@ -0,0 +1,11 @@
+package med.voll.api.paciente.record;
+
+import med.voll.api.paciente.Paciente;
+
+public record DadosListagemPaciente(Long id, String nome, String email, String cpf) {
+
+    public DadosListagemPaciente(Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+    }
+
+}
---
+++ src/main/java/med/voll/api/paciente/repository/PacienteRepository.java
@@ -0,0 +1,11 @@
+package med.voll.api.paciente.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import med.voll.api.paciente.Paciente;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
+}
