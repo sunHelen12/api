de8b82c550425f237a036278c166b73ef241ec91
sunHelen12 <sunrise.developer12@gmail.com>
2025-01-01 18:26:24 -0300

    Craindo Classe para tratamento de erro 404

--- src/main/java/med/voll/api/infra/TrantandoErros.java
+++ src/main/java/med/voll/api/infra/TrantandoErros.java
@@ -1,5 +1,17 @@
 package med.voll.api.infra;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
 public class TrantandoErros {
-    
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404(){
+        return ResponseEntity.notFound().build();
+    }
 }
