6e8d036cfbd213880450fb4bd13ca8cff3b0845e
sunHelen12 <sunrise.developer12@gmail.com>
2025-01-02 18:44:50 -0300

    Adição de li auth0 jwt para geração de tokens

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -23,7 +23,7 @@ public class AutenticacaoController {
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
-
-        return ResponseEntity.ok().build();
+        
+        return ResponseEntity.ok("");
     }
 }
