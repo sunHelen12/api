bd846ebeffe755812441a5e7840afa9268fb8780
sunHelen12 <sunrise.developer12@gmail.com>
2025-01-02 19:07:26 -0300

    Configurando classe e método para geração de token

---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,35 @@
+package med.voll.api.infra.security;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+
+import med.voll.api.domain.user.Usuario;
+
+@Service
+public class TokenService {
+    public String gerarToken(Usuario usuario){
+        try {
+            var algoritmo = Algorithm.HMAC256("2025");
+            return JWT.create()
+            .withIssuer("API voll.med")
+            .withSubject(usuario.getLogin())
+            .withClaim("id", usuario.getId())
+            .withExpiresAt(dataExpiracao())
+            .sign(algoritmo);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
