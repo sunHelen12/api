22f2cbdac458ef19aa1b82d7782e024608f05a47
sunHelen12 <sunrise.developer12@gmail.com>
2025-01-03 17:05:25 -0300

    Tratando erros e procurando ids do banco de dados

--- src/main/java/med/voll/api/domain/consultas/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consultas/AgendaDeConsultas.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Service;
 
 import med.voll.api.domain.consultas.records.DadosAgendamentoConsulta;
 import med.voll.api.domain.consultas.repository.ConsultaRepository;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.repository.MedicoRepository;
 import med.voll.api.domain.paciente.repository.PacienteRepository;
+import med.voll.api.infra.exception.ValidacaoException;
 
 @Service
 public class AgendaDeConsultas {
@@ -21,10 +23,21 @@ public class AgendaDeConsultas {
 
     public void agendar(DadosAgendamentoConsulta dados){
         
+        if(!medicoRepository.existsById(dados.idMedico())){
+            throw new ValidacaoException("ID do médico informado não existe!");
+        }
+        
+        if (dados.idMedico() != null && !pacienteRepository.existsById(dados.idMedico())) {
+            throw new ValidacaoException("ID do paciente informado não existe!");
+        }        
 
-        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var medico =  escolherMedico(dados);
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
         var consulta = new Consulta(null, medico, paciente, dados.data());
         consultaRepository.save(consulta);
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        
     }
 }
