0706b2252394cb919b4c3c72a11124f3f5461ae9
sunHelen12 <sunrise.developer12@gmail.com>
2025-01-02 21:50:39 -0300

    Validação de token para login de usuario

--- src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,8 +19,12 @@ public class SecurityConfiguration {
     @Bean    
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(req -> {
+                req.requestMatchers("/login").permitAll();
+                req.anyRequest().authenticated();
+            })
+            .build();
     }
 
     @Bean
